cace8f4204fef70da5bc1962cb6393f719f4a268
mindaman1101 <mindaman1101@ajou.ac.kr>
2026-09-04 11:47:08 +0900

    충돌 처리

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,8 @@
 #include "Player.h"
 #include "Engine.h"
 #include "InputDevice.h"
+#include "World.h"
+#include <vector>
 
 APlayer::APlayer()
 {
@@ -14,22 +16,45 @@ APlayer::~APlayer()
 
 void APlayer::Tick()
 {
-	//Player는 Engine의 InputDevice에게 현재 Key Code를 요청한다.
+	FVector2D NextLocation = Location;
+
 	int KeyCode = GEngine->GetInputDevice()->GetKeyCode();
 	if (KeyCode == 'w')
 	{
-		Location.Y--;  //Player는 위쪽으로 한 칸 이동한다.
+		NextLocation.Y--;  //위쪽으로 한 칸 이동
 	}
 	if (KeyCode == 's')
 	{
-		Location.Y++;  //Player는 아래쪽으로 한 칸 이동한다.
+		NextLocation.Y++;  //아래쪽으로 한 칸 이동
 	}
 	if (KeyCode == 'a')
 	{
-		Location.X--;  //Player는 왼쪽으로 한 칸 이동한다.
+		NextLocation.X--;  //왼쪽으로 한 칸 이동
 	}
 	if (KeyCode == 'd')
 	{
-		Location.X++;  //Player는 오른쪽으로 한 칸 이동한다.
+		NextLocation.X++;  //오른쪽으로 한 칸 이동
+	}
+
+	std::vector<AActor*>& Actors = GetWorld()->GetActors();
+
+	bool bCanMove = true;
+
+	for (AActor* Actor : Actors)
+	{
+		if (Actor == this || Actor->bIsCollisionEnable == false)  //Actor가 나거나 collision이 막혀있지 않다면 다음 Actor 확인
+		{
+			continue;
+		}
+		else if (Actor->Location.X == NextLocation.X && Actor->Location.Y == NextLocation.Y)  //Actor가 내가 아니고 collision이 막혀있을때 위치가 나랑 같으면 움직일 수 없음
+		{
+			bCanMove = false;
+			break;
+		}
+	}
+
+	if (bCanMove)
+	{
+		Location = NextLocation;
 	}
 }
--- Wall.cpp
+++ Wall.cpp
@@ -4,6 +4,7 @@ AWall::AWall()
 {
 	Shape = '*';  //Wall은 화면에서 *로 보인다.
 	Layer = 1;  //Wall은 Floor 다음에 그려진다.
+	bIsCollisionEnable = true;
 }
 
 AWall::~AWall()
